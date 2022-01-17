c92cd44b4948625592abb384ff79cfd07f9c704f
100007217 <gomezmonterroso14@gmail.com>
2022-01-17 17:03:18 +0100

    ajax un poco

---
+++ public/js/ajax.js
@@ -0,0 +1,57 @@
+function objetoAjax() {
+    var xmlhttp = false;
+    try {
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        try {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (E) {
+            xmlhttp = false;
+        }
+    }
+    if (!xmlhttp && typeof XMLHttpRequest != 'undefined') {
+        xmlhttp = new XMLHttpRequest();
+    }
+    return xmlhttp;
+}
+alert('random');
+/* Función para filtrar recursos implementada con AJAX */
+function filtro() {
+    /* Obtener elemento html donde introduciremos la recarga (datos o mensajes) */
+    var table = document.getElementById('table');
+
+    /* Obtener elemento/s que se pasarán como parámetros: token, method, inputs... */
+    var token = document.getElementById('token').getAttribute("content");
+    var method = document.getElementById('postFiltro').value
+    var filtro = document.getElementById('search').value
+
+    /*Usar el objeto FormData para guardar los parámetros que se enviarán:*/
+
+    var formData = new FormData();
+    formData.append('_token', token);
+    formData.append('_method', method);
+    formData.append('nombre', filtro);
+
+    /* Inicializar un objeto AJAX */
+    var ajax = objetoAjax();
+    /*
+    ajax.open("method", "rutaURL", true);
+    GET  -> No envía parámetros
+    POST -> Sí envía parámetros
+    true -> asynchronous
+    */
+    ajax.open("POST", "/clientes/shows", true);
+    ajax.onreadystatechange = function() {
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                var respuesta = JSON.parse(this.responseText);
+                /* Crear la estructura html que se devolverá dentro de una variable recarga*/
+                var recarga = '';
+                /* creación de estructura: la estructura que creamos no ha de contener código php ni código blade*/
+                /* utilizamos innerHTML para introduciremos la recarga en el elemento html pertinente */
+            }
+        }
+        /*
+        send(string)->Sends the request to the server (used for POST)
+        */
+    ajax.send(formData)
+}
--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>App gestor de clientes | CRUD Laravel + AJAX</title>
+    <script src="js/ajax.js"></script>
+    <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
 </head>
 <body>
     <h1 class="p-4">App gestor de clientes</h1>
@@ -14,25 +16,25 @@
         <div class="row pb-3">
             <div class="col">
                 {{-- Buscador (filtro) --}}
-                <form action="{{route('clientes.shows')}}" method="post">
-                    @csrf
-                    {{method_field('POST')}}
+                <form method="post">
+                    
+                    <input type="hidden" name="_method" value="POST" onkeyup="filtro(); return false;" id="postFiltro">
                     <div class="form-outline">
-                        <input type="search" id="" name="nombre" class="form-control" placeholder="Buscar por nombre..." aria-label="Search" />
+                        <input type="search" id="search" name="nombre" class="form-control" placeholder="Buscar por nombre..." aria-label="Search" />
                     </div>
                 </form>
             </div>
             <div class="col">
                 {{-- Route::get('/clientes/create',[ClienteController::class,'create'])->name('clientes.create'); --}}
                 <form action="{{route('clientes.create')}}" method="post">
-                    @csrf
-                    {{method_field('GET')}}
+                    
+                    <input type="hidden" name="_method" value="GET">
                     <input type="submit" class="btn btn-primary" value="Nuevo cliente">
                 </form>
             </div>
             <div class="col"></div>
         </div>       
-        <table class="table">
+        <table class="table" id="table">
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Nombre</th>
@@ -51,16 +53,16 @@
                 <td>
                     {{-- Route::get('/clientes/{cliente}/edit',[ClienteController::class,'edit'])->name('clientes.edit'); --}}
                     <form action="{{route('clientes.edit',['cliente'=>$cliente->id])}}" method="post">
-                        @csrf
-                        {{method_field('GET')}}
+                        
+                        <input type="hidden" name="_method" value="GET">
                         <button class= "btn btn-secondary" type="submit" value="Edit">Editar</button>
                     </form>
                 </td>
                 <td>
                     {{-- Route::delete('/clientes/{cliente}',[ClienteController::class,'destroy'])->name('clientes.destroy'); --}}
-                    <form action="{{route('clientes.destroy',['cliente'=>$cliente->id])}}" method="post">
-                        @csrf
-                        {{method_field('DELETE')}}
+                    <form method="post">
+                        
+                        <input type="hidden" name="_method" value="DELETE">
                         <button class= "btn btn-danger" type="submit" value="Delete">Eliminar</button>
                     </form>
                 </td>
